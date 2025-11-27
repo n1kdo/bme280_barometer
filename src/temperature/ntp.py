8efa583332fee3072c570fb18c4cd08493b6006e
n1kdo <otterson@gmail.com>
2025-11-27 12:46:26 -0500

    latest utils.py ntp.py

--- src/temperature/ntp.py
+++ src/temperature/ntp.py
@@ -1,43 +1,83 @@
+#
+# NTP client for MicroPython and CPython
+# This module provides a function to get the current time from an NTP server.
+#
+__author__ = 'J. B. Otterson'
+__copyright__ = 'Copyright 2024, 2025 J. B. Otterson N1KDO.'
+__version__ = '0.0.9'
+#
+# Copyright 2024, 2025, J. B. Otterson N1KDO.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import socket
 import struct
 import sys
 import time
 
-if sys.implementation.name == 'micropython':
-    import machine
+_IS_MICROPYTHON = sys.implementation.name == 'micropython'
 
-UNIX_EPOCH = 2208988800  # 1970-01-01 00:00:00
+if _IS_MICROPYTHON:
+    import micro_logging as logging
+    from machine import RTC
+    _rtc = RTC()
+else:
+    import logging
+    _rtc = None
+    def const(i):
+        return i
 
+_UNIX_EPOCH = const(2208988800)  # 1970-01-01 00:00:00
+_NTP_PORT = const(123)
+_BUF_SIZE = const(1024)
+_NTP_MSG = b'\x1b' + b'\0' * 47
+_SOCKET_TIMEOUT = const(5)
+_STRUCT_FORMAT = '!12I'
 
 def get_ntp_time(host='pool.ntp.org'):
-    port = 123
-    buf = 1024
+    sock = None
     try:
-        address = socket.getaddrinfo(host, port)[0][-1]
-        msg = b'\x1b' + 47 * b'\0'
-
+        address = socket.getaddrinfo(host, _NTP_PORT)[0][-1]
         # connect to server
-        client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        client.settimeout(5)
-        client.sendto(msg, address)
-        msg, address = client.recvfrom(buf)
-    except OSError as ose:
-        print(ose)
-        return None
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(_SOCKET_TIMEOUT)
+        sock.sendto(_NTP_MSG, address)
+        msg = sock.recvfrom(_BUF_SIZE)[0]
+        sock.close()
 
-    t = struct.unpack('!12I', msg)[10]
-    # reference time (in seconds since 1900-01-01 00:00:00)
-    t -= UNIX_EPOCH
-    tt = time.gmtime(t)
-    if sys.implementation.name == 'micropython':
-        # set the RTC
-        rtc = machine.RTC()
-        ttt = (tt[0], tt[1], tt[2], tt[6], tt[3], tt[4], tt[5], 0)
-        try:
-            rtc.datetime(ttt)
-        except OSError as e:
-            print(e)
-    return tt
+        t = struct.unpack(_STRUCT_FORMAT, msg)[10] - _UNIX_EPOCH
+        tt = time.gmtime(t)
+        if _IS_MICROPYTHON:
+            # set the RTC
+            try:
+                _rtc.datetime((tt[0], tt[1], tt[2], tt[6], tt[3], tt[4], tt[5], 0))
+            except OSError as ose:
+                logging.exception('OSError', 'ntp.get_ntp_time', ose)
+        return tt
+    except OSError as ose:
+        logging.exception('OSError', 'ntp.get_ntp_time', ose)
+        return None
+    finally:
+        if sock:
+            sock.close()
 
 
 def main():
--- src/temperature/utils.py
+++ src/temperature/utils.py
@@ -3,7 +3,7 @@
 #
 __author__ = 'J. B. Otterson'
 __copyright__ = """
-Copyright 2023, J. B. Otterson N1KDO.
+Copyright 2023, 2025 J. B. Otterson N1KDO.
 Redistribution and use in source and binary forms, with or without modification, 
 are permitted provided that the following conditions are met:
   1. Redistributions of source code must retain the above copyright notice, 
@@ -22,13 +22,14 @@ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 
 import sys
 import time
 
 upython = sys.implementation.name == 'micropython'
 
+BITS = bytes([0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4])
 
 def get_timestamp(tt=None):
     if tt is None:
@@ -41,7 +42,22 @@ def milliseconds():
 
 
 def safe_int(value, default=-1):
+    if value is None:
+        return default
     if isinstance(value, int):
         return value
-    return int(value) if value.isdigit() else default
+    try:
+        return int(value)
+    except ValueError:
+        return default
+
+
+def num_bits_set(n: int) -> int:
+    #       0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1111
+    nn = n
+    set_bits = 0
+    while nn:
+        set_bits += BITS[nn & 0x0f]
+        nn >>= 4
+    return set_bits
 
